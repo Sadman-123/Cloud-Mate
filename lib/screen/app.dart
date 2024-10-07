e91b5a06dd5c8935fb19e082379504c7cfd47bfd
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-10-07 07:36:04 +0600

    set up folder structure and initial the providers

---
+++ lib/screen/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Text("Hello World"),
+      ),
+    );
+  }
+}
